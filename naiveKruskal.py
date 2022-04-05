055a1a83ac3e092b15e7dff59dc71e4e127c7ff2
Carter Vande Moore <72159057+cvmoore@users.noreply.github.com>
2022-04-05 15:44:42 +0200

    Updated naiveKruskal

--- naiveKruskal.py
+++ naiveKruskal.py
@@ -1,7 +1,7 @@
 # Python implementation for Kruskal's algorithm
 
 # Find set of vertex a
-def find(a):
+def find(a, parent):
     while parent[a] != a:
         a = parent[a]
     return a
@@ -9,13 +9,26 @@ def find(a):
 # Does union of a and b. It returns
 # false if a and b are already in same
 # set.
-def union(a, b):
-    node1 = find(a)
-    node2 = find(b)
+def union(a, b, parent):
+    node1 = find(a, parent)
+    node2 = find(b, parent)
     parent[node1] = parent[node2]
 
 def kruskalMST(cost):
-    costTotal = 0 
+
+    #Create the graph
+    numNodes = cost[0][0]
+    parent = [node for node in range(numNodes)]
+    costs = []
+    for iterate in range(numNodes):
+        costs.append([INF]*numNodes)
+    print(costs)
+    for iterate in range(1, len(cost)):
+        num1 = cost[iterate][0]-1
+        num2 = cost[iterate][1]-1
+        costs[num1][num2] = cost[iterate][2]
+        costs[num2][num1] = cost[iterate][2]
+        costTotal = 0 
 
     # Initialize sets of disjoint sets
     for node in range(numNodes):
@@ -29,13 +42,19 @@ def kruskalMST(cost):
         b = -1
         for i in range(numNodes):
             for j in range(numNodes):
-                if find(i) != find(j) and cost[i][j] < min:
-                    min = cost[i][j]
+                if find(i, parent) != find(j, parent) and costs[i][j] < min:
+                    min = costs[i][j]
                     a = i
                     b = j
-        union(a, b)
+        union(a, b, parent)
         print("Edge " + str(edge) + ": (" + str(a) + "," + str(b) + ")" + " cost:" + str(min))
         edge += 1
         costTotal += min
     print("Minimum cost= " + str(costTotal))
     return costTotal
+
+INF = float('inf')
+cost = [[10,9], [1,2,4993], [2,3,1392], [3,4,8856], [4,5,-433], [5,6,6590], [6,7,-7462], [7,8,6658], [8,9,-976], [9, 10,9698]]
+
+# Print the solution
+kruskalMST(cost)
